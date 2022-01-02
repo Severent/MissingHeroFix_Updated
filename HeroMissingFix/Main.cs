9596f5cf53cc552c0dd8911754a67d35db1ae2c0
jonat <jonat@JONATHANDESKTOP>
2022-01-02 11:43:16 -0600

    Add project files.

---
+++ HeroMissingFix/Main.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using TaleWorlds.Core;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Library;
+using TaleWorlds.MountAndBlade;
+
+namespace HeroMissingFix
+{
+    // Main inherits from MBSubMOduleBase
+    public class HeroFixSubModule : MBSubModuleBase
+    {
+        public static readonly Color textColor = Color.FromUint(6750401U);
+
+        protected override void OnBeforeInitialModuleScreenSetAsRoot()
+        {
+            base.OnBeforeInitialModuleScreenSetAsRoot();
+            InformationManager.DisplayMessage(new InformationMessage("Successfully loaded HeroMissingFix.", textColor));
+        }
+
+        protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
+        {
+            base.OnGameStart(game, gameStarterObject);
+
+            if (game.GameType is Campaign)
+            {
+                CampaignGameStarter gameInitializer = (CampaignGameStarter)gameStarterObject;
+                try
+                {
+                    gameInitializer.AddBehavior(new ScheduledEvents());
+                }
+                catch (Exception ex)
+                {
+                    InformationManager.DisplayMessage(new InformationMessage($"Error while initialising HeroMissingFix:\n\n { ex.Message} \n\n { ex.StackTrace}", textColor));
+                }
+            }
+        }
+
+
+    }
+}
---
+++ HeroMissingFix/ScheduledEvents.cs
@@ -0,0 +1,45 @@
+﻿using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+
+namespace HeroMissingFix
+{
+    internal class ScheduledEvents : CampaignBehaviorBase
+	{
+		public override void RegisterEvents()
+		{
+			CampaignEvents.DailyTickEvent.AddNonSerializedListener(this, DailyPrisonerCheck);
+		}
+
+        public override void SyncData(IDataStore dataStore)
+        {
+        }
+
+        private void DailyPrisonerCheck()
+		{
+			InformationManager.DisplayMessage(new InformationMessage("Checking for Missing Heroes..."));
+
+
+			// Every day check if any Heros are alive but disabled and it belongs to a clan.
+			// Pretty sure I could remove the tutorial and storymode NPCs (they have that prefixed in name) since I'm checking clan,
+			// but leaving there since it doesn't hurt anything and was an edge case I hit before adding clan check.
+			foreach (Hero hero in Hero.FindAll(hero => !hero.IsHumanPlayerCharacter && hero.IsAlive && hero.IsDisabled && hero.Clan != null 
+						&& !hero.GetName().Contains("tutorial") && !hero.GetName().Contains("storymode")))
+			{
+				string clanName = hero.Clan != null ? hero.Clan.FullName.ToString() : "";
+
+				// Log some info in chat about hero fixing to get reactivated
+				InformationManager.DisplayMessage(new InformationMessage("========"));
+				InformationManager.DisplayMessage(new InformationMessage($"Missing: {hero.GetName()}, {hero.ToString()}"));
+				InformationManager.DisplayMessage(new InformationMessage($"Missing IsAlive: {hero.IsAlive}"));
+				InformationManager.DisplayMessage(new InformationMessage($"Missing State: {hero.HeroState}"));
+				InformationManager.DisplayMessage(new InformationMessage($"Missing IsPartyLeader: {hero.IsPartyLeader}, CanLead: {hero.CanLeadParty()}"));
+				InformationManager.DisplayMessage(new InformationMessage($"Missing Clan: {clanName}"));
+				InformationManager.DisplayMessage(new InformationMessage($"Missing Age: {hero.Age}"));
+				InformationManager.DisplayMessage(new InformationMessage($"Missing LastSeen: {hero.LastSeenTime.ElapsedDaysUntilNow}"));
+
+				// Fix hero
+				hero.ChangeState(Hero.CharacterStates.Released);
+			}
+         }
+	}
+}
